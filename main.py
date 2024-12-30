1c06e748e397235cdc373c03aff9621bda08ef1d
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2024-12-30 09:20:16 +0100

    Add SMARD check

--- main.py
+++ main.py
@@ -13,25 +13,60 @@ def write_message(message):
     with open(f"{checkson_dir}/message", "w") as f:
         f.write(message)
 
-url = "https://api.gcmb.io/api/orgs/adsb/projects/adsb/publish-stats"
 
-r = requests.get(url)
-r.raise_for_status()
+def check_stat_timestamp(stats, max_minutes):
+    last_timestamp = datetime.fromisoformat(stats['lastTimestamp'])
+    # Check if the last timestamp is within the last max_minutes minutes
+    return last_timestamp.astimezone(timezone.utc) > datetime.now().astimezone(timezone.utc) - timedelta(minutes=max_minutes)
 
-stats = r.json()
 
-# Parse as ISO 8601 date
-last_timestamp = datetime.fromisoformat(stats['lastTimestamp'])
+def check_adsb():
+    url = "https://api.gcmb.io/api/orgs/adsb/projects/adsb/publish-stats"
 
-# Check if the last timestamp is within the last 5 minutes
-if last_timestamp.astimezone(timezone.utc) < datetime.now().astimezone(timezone.utc) - timedelta(minutes=5):
-    write_message(f"Last timestamp is {last_timestamp}, which is more than 5 minutes ago")
-    sys.exit(1)
+    r = requests.get(url)
+    r.raise_for_status()
 
-number_of_messages = int(stats['prevMinuteCount'])
+    stats = r.json()
 
-if number_of_messages < 100:
-    write_message(f"Number of messages in last minute is {number_of_messages}, which is less than 100")
-    sys.exit(1)
+    if not check_stat_timestamp(stats, 5):
+        write_message(f"Last timestamp is {stats['lastTimestamp']}, which is more than 5 minutes ago")
+        sys.exit(1)
 
-write_message(f"Check successful. Stats: {json.dumps(stats)}")
+    number_of_messages = int(stats['prevMinuteCount'])
+
+    if number_of_messages < 100:
+        write_message(f"Number of messages in last minute is {number_of_messages}, which is less than 100")
+        sys.exit(1)
+
+    print(f"ADS-B check successful, stats: {stats}")
+
+
+def check_smard():
+    url = "https://api.gcmb.io/api/orgs/stefan/projects/smard/publish-stats"
+
+    r = requests.get(url)
+    r.raise_for_status()
+
+    stats = r.json()
+
+    if not check_stat_timestamp(stats, 30):
+        write_message(f"Last timestamp is {stats['lastTimestamp']}, which is more than 30 minutes ago")
+        sys.exit(1)
+
+    number_of_messages = int(stats['prevHourCount'])
+
+    if number_of_messages < 100:
+        write_message(f"Number of messages in previous hour is {number_of_messages}, which is less than 100")
+        sys.exit(1)
+
+    print(f"SMARD check successful, stats: {stats}")
+
+
+def main():
+    check_adsb()
+    check_smard()
+    write_message(f"All checks successful")
+
+
+if __name__ == "__main__":
+    main()
